4ff4a1385704f7e7e5978351ffe564adb707f56b
giscafer <giscafer@outlook.com>
2021-04-14 15:50:19 +0800

    fix: 图片搜索逻辑，保证正确性

--- src/modules/searchPic.js
+++ src/modules/searchPic.js
@@ -1,33 +1,51 @@
 //http://image.so.com/j?q=curry&src=srp&correct=curry&sn=30&pn=1
 
-
 const urlencode = require('urlencode');
+const Axios = require('axios');
+const { isImage } = require('../utils/image');
 
 let searchUrl = (key) => {
-    return `http://image.so.com/j?q=${urlencode(key)}&src=srp&correct=${urlencode(key)}&sn=30&pn=100`;
-}
+  return `http://image.so.com/j?q=${urlencode(key)}&src=srp&correct=${urlencode(
+    key,
+  )}&sn=30&pn=15`;
+};
 
 const randomIndex = (length) => {
-    return Math.ceil(Math.random() * length);
-}
+  return Math.ceil(Math.random() * length);
+};
 
 function getPicture(key) {
-    key = key.trim();
-    let url = searchUrl(key);
-    // console.log(url);
-    return new Promise((resolve, reject) => {
-        fetch(url).then(res => res.json()).then(json => {
-            const { length } = json.list;
-            const item = json.list[randomIndex(length)];
-            return resolve(item.img);
-        }).catch(err => {
-            console.log(err);
-            return reject(err)
+  key = key.trim();
+  let url = searchUrl(key);
+  console.log(url);
+  return new Promise((resolve, reject) => {
+    Axios.get(url)
+      .then((resp) => {
+        const json = resp.data;
+        const result = json.list || [];
+
+        const imgUrlList = [];
+        result.forEach((item) => {
+          if (item.img) {
+            if (isImage(item.img) && item.img.length < 90) {
+              imgUrlList.push(item.img);
+            }
+          }
         });
-    });
+        let targetImgUrl = '';
+        if (imgUrlList.length > 0) {
+          targetImgUrl = imgUrlList[randomIndex(imgUrlList.length - 1)];
+        }
+        resolve(targetImgUrl);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
 }
 
-module.exports = getPicture
+module.exports = getPicture;
 
 // test
-// getPicture('').then(json => console.log(json));
+// getPicture('周六唱歌').then((json) => console.log(json));
