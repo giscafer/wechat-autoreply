b8f62b277c8cd7832f44150f061a05c2a75be836
giscafer <giscafer@outlook.com>
2021-05-11 22:24:51 +0800

    update src/config.js.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 //指定某些群或者对象可以起作用，其他人不行
-const activeRooms = ['回本销号互助群', 'ClownHub', 'LeekHub测试群', 'LeekHub'];
+const activeRooms = ['回本销户互助群', 'ClownHub', 'LeekHub测试群', 'LeekHub'];
 
 module.exports = { activeRooms };
